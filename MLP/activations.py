34b2804dd5a42815864c86396cdaa235447f6df9
niksonber <nik.son18@hotmail.com>
2022-08-21 20:36:58 +0000

    Activation as layers

--- MLP/activations.py
+++ MLP/activations.py
@@ -1,15 +1,28 @@
 import numpy as np
 
-
-def linear(input):
-    return input
+from layers import Layer
 
 
-def sigmoid(input):
-    return 1 / (1 + np.exp(-input))
+class Linear(Layer):
+    def __init__(self, training=True) -> None:
+        super().__init__(training, False)
+
+    def __call__(self, X):
+        self.record(X)
+        return X
+
+    def backwards(self, grads):
+        return grads
 
 
-ACTIVATION_DICT = {
-    'linear': linear,
-    'sigmoid': sigmoid
-}
+class Sigmoid(Layer):
+    def __init__(self, training=True) -> None:
+        super().__init__(training, False)
+
+    def __call__(self, X):
+        self.record(X)
+        return 1 / (1 + np.exp(-X))
+
+    def backwards(self, grads):
+        output = self(self._last_input)
+        return grads * output * (1 - output)
