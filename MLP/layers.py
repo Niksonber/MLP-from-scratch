ecc3bba41b35c7624a7135e3897123d6cb976ac9
niksonber <nik.son18@hotmail.com>
2022-08-21 20:38:55 +0000

    Dense layer as just fully-connected, adding backpropagation method

--- MLP/layers.py
+++ MLP/layers.py
@@ -2,12 +2,25 @@ from abc import ABC, abstractmethod
 
 import numpy as np
 
-from activations import ACTIVATION_DICT
-
 
 class Layer(ABC):
+    def __init__(self, training=True, trainable=True) -> None:
+        super().__init__()
+
+        self.trainable = trainable
+        self.training = training
+        self._last_input = None
+
+    def record(self, X):
+        if self.training:
+            self._last_input = X
+
     @abstractmethod
     def __call__(self, X):
+        pass
+
+    @abstractmethod
+    def backwards(self, grads):
         pass
 
 
@@ -15,20 +28,24 @@ class Dense(Layer):
     """
     Abstraction of fully-connected layer
     """
-    def __init__(self, n_neurons: int, input_size: int, activation='sigmoid') -> None:
-        super().__init__()
+    def __init__(
+        self,
+        n_neurons: int,
+        input_size: int,
+        training=True
+    ) -> None:
+        super().__init__(training)
 
         # Bias is defined as the weight from an aditional input with value 1
         self.weigths = np.random.random(size=(n_neurons, input_size + 1))
-        self.activation = ACTIVATION_DICT[activation]
 
     @classmethod
-    def from_weights(cls, weights, activation='sigmoid'):
+    def from_weights(cls, weights):
         """
         Init Dense layer from weights
         """
         weights = np.array(weights)
-        instance = cls(weights.shape[0], weights.shape[1] - 1, activation)
+        instance = cls(weights.shape[0], weights.shape[1] - 1)
         instance.weigths = np.array(weights)
 
         return instance
@@ -43,4 +60,19 @@ class Dense(Layer):
         # forces input to have 2d shape (batch_size x input_size)
         # and insert artificial input 1 to bias weight
         X = np.insert((X if X.ndim > 1 else X[np.newaxis]), 0, values=1.0, axis=1)
-        return self.activation(X @ self.weigths.T)
+
+        # if is in training mode, store last input to backpropagation step
+        self.record(X)
+        return X @ self.weigths.T
+
+    def backwards(self, grads):
+        """
+        Backwards gradients
+        """
+        # (batch_size, n_neurons) x (n_neurons, n_inputs) -> (batch_size, n_inputs)
+        grad_inputs = grads @ self.weigths[:, 1:]
+
+        # (n_neurons, batch_size) x (batch_size, n_inputs) -> (n_neurons, n_inputs)
+        self.grad = grads.T @ self._last_input
+
+        return grad_inputs
