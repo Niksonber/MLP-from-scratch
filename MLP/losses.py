60e497a62eed68642468f77a099399cc1cc4aeb3
niksonber <nik.son18@hotmail.com>
2022-08-22 00:54:43 +0000

    adding sparse catecorical cross-entropy loss

--- MLP/losses.py
+++ MLP/losses.py
@@ -22,3 +22,22 @@ class CategoricalCrossEntropy:
 
         return np.mean(np.sum(-y_true * np.log(y_pred), axis=1))
 
+
+class SparseCategoricalCrossEntropy(CategoricalCrossEntropy):
+    def __call__(self, logits: np.ndarray, y_true: np.ndarray) -> np.ndarray:
+        """
+        Calculate sparse categorical cross-entropy from logits
+
+        Parameters
+        ---------
+
+        logits :
+            logits array with shape (batch_size, n_outputs)
+
+        y_true :
+           labels (shape: (batch_size, ))
+        """
+        y_one_hot_encoded = np.zeros(shape=logits.shape)
+        y_one_hot_encoded[np.arange(len(y_true)), y_true] = 1.0
+
+        return super().__call__(logits, y_one_hot_encoded)
